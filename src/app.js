5e4933cd36fa553c429c1234703562966cda04a4
bradcstevens <brad@bradcstevens.com>
2017-11-14 10:47:29 -0800

    added sample createTicket function

--- src/app.js
+++ src/app.js
@@ -8,48 +8,81 @@ var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function() {
     console.log('%s listening to %s', server.name, server.url);
 });
+
 console.log(process.env)
-    // Create chat bot
+
+// Create chat bot
 var connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
     appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 
-const ChangePasswordOption = 'Change Password';
-const ResetPasswordOption = 'Reset Password';
+// Listen for messages
+server.post('/api/messages', connector.listen());
 
-var bot = new builder.UniversalBot(connector, [
-    (session) => {
-        builder.Prompts.choice(session,
-            'What do yo want to do today?', [ChangePasswordOption, ResetPasswordOption], { listStyle: builder.ListStyle.button });
-    },
-    (session, result) => {
-        if (result.response) {
-            switch (result.response.entity) {
-                case ChangePasswordOption:
-                    session.send('This functionality is not yet implemented! Try resetting your password.');
-                    session.reset();
-                    break;
-                case ResetPasswordOption:
-                    session.beginDialog('resetPassword:/');
-                    break;
-            }
-        } else {
-            session.send(`I am sorry but I didn't understand that. I need you to select one of the options below`);
-        }
-    },
-    (session, result) => {
-        if (result.resume) {
-            session.send('You identity was not verified and your password cannot be reset');
-            session.reset();
-        }
+var bot = new builder.UniversalBot(connector, '/');
+
+var model = 'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/' + process.env.LUIS_ID + '?subscription-key=' + process.env.LUIS_KEY + '&verbose=true&timezoneOffset=0&q='
+var recognizer = new builder.LuisRecognizer(model)
+var dialog = new builder.IntentDialog({ recognizers: [recognizer] });
+
+myObj = { 'username': 'null', 'issueType': 'null', 'deviceType': 'null', 'macaddr': 'null', 'resHall': 'null' };
+var isDone = false;
+
+bot.dialog('/', dialog)
+
+    //========================
+    //LUIS Dialog
+    //========================
+
+    //==========
+    //Help!
+    //==========
+    .matches('help', helpResponse)
+    .matches('greeting', helpResponse)
+
+    //==========
+    //Create Tickets
+    //==========
+    .matches('create', createTicket)
+    .matches('new', createTicket)
+    .matches('issue', createTicket)
+
+
+    //==========
+    //reset in event of wrong answers
+    //==========
+    .matches('restart', restartResponse)
+    .onDefault((session, results) => {
+        session.send("Sorry, I didn't understand");
+    })
+
+    //==========
+    //Help!
+    //==========
+    function helpResponse(session)
+    {
+        session.send("I'm your Library Digital Assistant to help you get your issue to someone who can help. What appears to be the problem?")
     }
-]);
 
-//Sub-Dialogs
-bot.library(require('./dialogs/reset-password'));
+    //==========
+    //Base issue
+    //==========
+    function createTicket(session)
+    {
+        session.send("First things first, what seems to be the problem?");
+    }
 
-//Validators
-bot.library(require('./validators'));
+    //==========
+    //reset in event of wrong answers
+    //==========
+    function restartResponse(session)
+    {
+        for (x in myObj)
+            {
+                myObj[x] = "null";
+            }
+        session.send("Sorry for the mistake. We've cleared all our data again, please describe your issue again.")
+    }
 
 server.post('/api/messages', connector.listen());
