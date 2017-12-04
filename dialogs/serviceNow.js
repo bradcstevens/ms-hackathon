a342d287a0861367f977e185946907a226c3810b
Brad Stevens <brad.stevens@cdw.com>
2017-12-04 10:19:49 -0800

    Update ServiceNow Config

--- dialogs/serviceNow.js
+++ dialogs/serviceNow.js
@@ -1,6 +1,6 @@
 let axios = require('axios')
 
-let auth = `Basic ${Buffer.from("admin:Password999!").toString('base64')}`;
+let auth = `Basic ${Buffer.from("admin:SdWKd5Fyr3WU").toString('base64')}`;
 
 const config = {
     headers: {
@@ -10,7 +10,7 @@ const config = {
 };
 
 const createTicket = (dialogData, callerId) => {
-    let route = "https://dev45236.service-now.com/api/now/v1/table/incident?sysparm_suppress_auto_sys_field=true";
+    let route = "https://dev39812.service-now.com/api/now/v1/table/incident?sysparm_suppress_auto_sys_field=true";
     let ticket = {
         caller_id: callerId,
         short_description: dialogData.short_description,
@@ -25,12 +25,12 @@ const createTicket = (dialogData, callerId) => {
 }
 
 const getTicketByNumber = async(ticketNumber) => {
-    let route = `https://dev45236.service-now.com/api/now/v1/table/incident?sysparm_query=number%3D${ticketNumber}`;
+    let route = `https://dev39812.service-now.com/api/now/v1/table/incident?sysparm_query=number%3D${ticketNumber}`;
     return axios.get(route, config);
 }
 
 const listTickets = async(callerId) => {
-    let route = `https://dev45236.service-now.com/api/now/table/incident?sysparm_query=caller_id%3D${callerId}`;
+    let route = `https://dev39812.service-now.com/api/now/table/incident?sysparm_query=caller_id%3D${callerId}`;
     return axios.get(route, config)
 }
 
@@ -39,7 +39,7 @@ const closeTicket = (ticket) => {
     return axios.post(route, ticket, config)
 }
 const updateTicket = (ticketID, notes, userId) => {
-    let route = `https://dev45236.service-now.com/api/now/v1/table/task/${ticketID}?sysparm_exclude_ref_link=true`;
+    let route = `https://dev39812.service-now.com/api/now/v1/table/task/${ticketID}?sysparm_exclude_ref_link=true`;
     let updateTicket = {
         caller_id: userId,
         /* short_description: ticket.short_description, */
@@ -59,7 +59,7 @@ const reOpenTicket = (ticket) => {
 }
 
 const getUserRecord = (firstName, lastName) => {
-    let route = `https://dev45236.service-now.com/api/now/v1/table/sys_user?sysparm_query=first_name%3D${firstName}%5Elast_name%3D${lastName}`;
+    let route = `https://dev39812.service-now.com/api/now/v1/table/sys_user?sysparm_query=first_name%3D${firstName}%5Elast_name%3D${lastName}`;
     return axios.get(route, config);
 }
 
